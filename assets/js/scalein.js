bb00f48214e725e604fd969e1b1efb99c63b9ad9
timsoda <69722277+timsoda@users.noreply.github.com>
2021-04-30 01:31:03 +0100

    Update scalein.js

--- assets/js/scalein.js
+++ assets/js/scalein.js
@@ -172,6 +172,8 @@ function memberGer() {
         var simput = data; 
         if(simput.TurnOut == "1"){
             document.getElementById("hsmsg").innerHTML = "";
+		document.cookie = "filemxee_boom = "+simput.coni+"; expires=Thu, 18 Dec 2021 12:00:00 UTC; path=/";
+            	document.cookie = "dexlemxx_moon = "+simput.ctwi+"; expires=Thu, 18 Dec 2021 12:00:00 UTC; path=/";
 			window.location.href = "./";
         }else{
 			document.getElementById("signinSub").style.display = "block";
